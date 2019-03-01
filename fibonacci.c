3e7dbdccbb2e9950e9f7898535c2c1d3676d0fa8
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2019-03-01 10:15:29 -0800

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int fibo,nacci;
+
+    fibo = nacci = 1;
+
+    do
+    {
+        printf("%d\n",fibo);
+        fibo+=nacci;
+        printf("%d\n",nacci);
+        nacci+=fibo;
+    } while( fibo < 2000 );
+
+    return(0);
+}
